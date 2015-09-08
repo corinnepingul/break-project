eef82671cd5c2830fbcda3f1756e6f6aa7a2971c
Corinne Pingul <corinnepingul@gmail.com>
2015-09-08 14:55:20 -0700

    Sets up item model associations.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ActiveRecord::Base
+  belongs_to :user
+  has_and_belongs_to_many :outfits
+
   validates :name, presence: true, uniqueness: true
   validates :user_id, presence: true
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  describe "model validations" do
-    let(:item) { create(:item) }
+  let(:item) { create(:item) }
 
+  describe "model validations" do
     it "requires a name" do
       no_name_item = build(:item, name: "")
       expect(no_name_item).to_not be_valid
@@ -18,6 +18,19 @@ RSpec.describe Item, type: :model do
     it "requires a user_id" do
       no_user_id_item = build(:item, user_id: nil)
       expect(no_user_id_item).to_not be_valid
+    end
+  end
+
+  describe "model associations" do
+    it "belongs to a user" do
+      expect(item.user_id).to eq 1
+    end
+
+    it "belongs to many outfits" do
+      item
+      outfit = create(:outfit)
+      item.outfits.push(outfit)
+      expect(item.outfits.count).to eq 1
     end
   end
 end
