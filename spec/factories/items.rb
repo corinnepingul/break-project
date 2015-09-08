d42c29461bfea3d883b72c4711105311ceed9785
Corinne Pingul <corinnepingul@gmail.com>
2015-09-08 14:03:51 -0700

    Formats factories

---
+++ spec/factories/items.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :item do
+    name "MyString"
+    description "MyText"
+    user_id 1
+  end
+end
---
+++ spec/factories/outfits.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :outfit do
+    name "MyString"
+    description "MyText"
+    user_id 1
+    category_id 1
+  end
+end
