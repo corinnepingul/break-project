bdd3811bb541ad54264f3825685c10b5e2a24ae1
Corinne Pingul <corinnepingul@gmail.com>
2015-09-08 14:29:26 -0700

    Sets up category model validations and specs

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  validates :user_id, presence: true
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "model validations" do
+    let(:category) { create(:category) }
+
+    it "requires a name" do
+      no_name_category = build(:category, name: "")
+      expect(no_name_category).to_not be_valid
+    end
+
+    it "requires a unique name" do
+      category
+      category2 = build(:category)
+      expect(category2).to_not be_valid
+    end
+
+    it "requires a user_id" do
+      no_user_id_category = build(:category, user_id: nil)
+      expect(no_user_id_category).to_not be_valid
+    end
+  end
 end
