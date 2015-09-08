8aade9c2434a3811911b080f1826dec60e48e17b
Corinne Pingul <corinnepingul@gmail.com>
2015-09-08 14:48:55 -0700

    Adds outfit model associations

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -1,4 +1,8 @@
 class Outfit < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :category
+  has_and_belongs_to_many :items
+
   validates :name, presence: true, uniqueness: true
   validates :user_id, presence: true
 end
--- spec/models/outfit_spec.rb
+++ spec/models/outfit_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Outfit, type: :model do
-  describe "model validations" do
-    let(:outfit) { create(:outfit) }
+  let(:outfit) { create(:outfit) }
 
+  describe "model validations" do
     it "requires a name" do
       no_name_outfit = build(:outfit, name: "")
       expect(no_name_outfit).to_not be_valid
@@ -18,6 +18,23 @@ RSpec.describe Outfit, type: :model do
     it "requires a user_id" do
       no_user_id_outfit = build(:outfit, user_id: nil)
       expect(no_user_id_outfit).to_not be_valid
+    end
+  end
+
+  describe "model associations" do
+    it "belongs to a user" do
+      expect(outfit.user_id).to eq 1
+    end
+
+    it "belongs to a category" do
+      expect(outfit.category_id).to eq 1
+    end
+
+    it "has and belongs to many items" do
+      outfit
+      item = create(:item)
+      outfit.items.push(item)
+      expect(outfit.items.count).to eq 1
     end
   end
 end
