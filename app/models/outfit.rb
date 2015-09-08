99b78fd03ff591ee489b212547e7a82b8ad9cef3
Corinne Pingul <corinnepingul@gmail.com>
2015-09-08 14:22:36 -0700

    Sets up outfit model specs and validations

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -1,2 +1,4 @@
 class Outfit < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  validates :user_id, presence: true
 end
--- spec/models/outfit_spec.rb
+++ spec/models/outfit_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Outfit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "model validations" do
+    let(:outfit) { create(:outfit) }
+
+    it "requires a name" do
+      no_name_outfit = build(:outfit, name: "")
+      expect(no_name_outfit).to_not be_valid
+    end
+
+    it "requires a unique name" do
+      outfit
+      outfit2 = build(:outfit)
+      expect(outfit2).to_not be_valid
+    end
+
+    it "requires a user_id" do
+      no_user_id_outfit = build(:outfit, user_id: nil)
+      expect(no_user_id_outfit).to_not be_valid
+    end
+  end
 end
