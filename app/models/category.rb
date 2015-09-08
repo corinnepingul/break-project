e71407cd18bef1918d3e7b746f84fc0dec70a58d
Corinne Pingul <corinnepingul@gmail.com>
2015-09-08 15:00:21 -0700

    Sets up category model associations.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ActiveRecord::Base
+  belongs_to :user
+  has_many :outfits
+
   validates :name, presence: true, uniqueness: true
   validates :user_id, presence: true
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  describe "model validations" do
-    let(:category) { create(:category) }
+  let(:category) { create(:category) }
 
+  describe "model validations" do
     it "requires a name" do
       no_name_category = build(:category, name: "")
       expect(no_name_category).to_not be_valid
@@ -18,6 +18,19 @@ RSpec.describe Category, type: :model do
     it "requires a user_id" do
       no_user_id_category = build(:category, user_id: nil)
       expect(no_user_id_category).to_not be_valid
+    end
+  end
+
+  describe "model associations" do
+    it "belongs to user" do
+      expect(category.user_id).to eq 1
+    end
+
+    it "has many outfits" do
+      category
+      outfit = create(:outfit)
+      category.outfits.push(outfit)
+      expect(category.outfits.count).to eq 1
     end
   end
 end
