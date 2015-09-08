0ab884547f0ff507b064d928193d06c4f96b2ca4
Corinne Pingul <corinnepingul@gmail.com>
2015-09-08 14:26:17 -0700

    Sets up item model validations and specs

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  validates :user_id, presence: true
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "model validations" do
+    let(:item) { create(:item) }
+
+    it "requires a name" do
+      no_name_item = build(:item, name: "")
+      expect(no_name_item).to_not be_valid
+    end
+
+    it "requires a unique name" do
+      item
+      item2 = build(:item)
+      expect(item2).to_not be_valid
+    end
+
+    it "requires a user_id" do
+      no_user_id_item = build(:item, user_id: nil)
+      expect(no_user_id_item).to_not be_valid
+    end
+  end
 end
